38c9896ce27e4764c3e39ee36d354844a612d36f
ijaz liaqat <ijazierpu@gmail.com>
2026-04-15 09:50:21 +0500

    feat: update Firebase configuration with actual credentials

--- src/services/config.js
+++ src/services/config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: "",
-    // authDomain: "",
-    // projectId: "",
-    // storageBucket: "",
-    // messagingSenderId:"",
-    // appId: ""
+   apiKey: "AIzaSyAo-cXupJrFAoOI1yR0skYy6LnEfH9tAV0",
+  authDomain: "my-app-5de4f.firebaseapp.com",
+  projectId: "my-app-5de4f",
+  storageBucket: "my-app-5de4f.firebasestorage.app",
+  messagingSenderId: "491864357662",
+  appId: "1:491864357662:web:0127ce89db4097568ae70f"
 };
 
 // Initialize Firebase
